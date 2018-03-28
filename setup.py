36eaa468ee069b69c5b44b38326c8b5d8753342f
MarkNepf <marknepf@gmail.com>
2018-03-28 14:49:09 -0400

    gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='multisatpm',
+      version='0.0.1',
+      description='Easy access to multisatpm data.',
+      url='http://github.com/dmsul/multisatpm',
+      packages=['multisatpm']
+      )
