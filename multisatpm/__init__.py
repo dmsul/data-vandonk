1b3416a5f62d855bec9827a260a0de26048be484
Daniel M. Sullivan <sullydm@gmail.com>
2018-05-12 16:24:13 -0400

    BUG: wrong function in __init__ for North Amer

--- multisatpm/__init__.py
+++ multisatpm/__init__.py
@@ -1,2 +1,2 @@
 from multisatpm.clean.raw import multisat_conus_year
-from multisatpm.clean.north_amer import msat_northamer_3year
+from multisatpm.clean.north_amer import msat_northamer_conus_3year
