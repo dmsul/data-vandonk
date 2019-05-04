9e4bf825297020fe06f6ca225639685ceefe3de5
Daniel Sullivan <sullydm@gmail.com>
2019-05-04 13:39:47 -0400

    REF imports to new package name

--- multisatpm/__init__.py
+++ multisatpm/__init__.py
@@ -1,3 +1,6 @@
-from multisatpm.clean.raw import multisat_conus_year
-from multisatpm.clean.north_amer import (msat_northamer_conus_3year,
-                                         msat_northamer_1year)
+# flake8: noqa
+from data_multisatpm.clean.raw import multisat_conus_year
+from data_multisatpm.clean.north_amer import (
+    msat_northamer_conus_3year,
+    msat_northamer_1year,
+)
